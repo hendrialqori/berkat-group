67745681212dc58b05dc395ab27b62598d32c401
Hendri Alqori <teamhendri18@gmail.com>
2024-12-27 13:28:47 +0700

    feat: add home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,33 +1,80 @@
-import { TbClockPin } from "react-icons/tb";
-import { RiClockwiseLine } from "react-icons/ri";
-import { Link } from "react-router";
+import { PiCashRegister } from "react-icons/pi";
+import { GrSchedules } from "react-icons/gr";
+import { TbReportMoney } from "react-icons/tb";
+import { HiOutlineOfficeBuilding } from "react-icons/hi";
 
 export default function Home() {
-
     return (
-        <main className="container font-inter flex flex-col justify-center items-center gap-10">
-            <header className="flex justify-center items-center">
-                <img src="/berkat-group-icon.png" className="" width={150} />
-            </header>
-            <section className="flex flex-col justify-center items-center gap-6">
-                <div className="text-center">
-                    <h1 className="text-sm font-semibold -tracking-wide">Selamat datang di Berkat group</h1>
-                    <p className="text-xs font-medium text-gray-500">Silahkan pilih tipe absen anda di bawah</p>
-                </div>
-                <div className="text-xs font-medium w-full space-y-3">
-                    <Link to={{ pathname: "/clock-in" }} className="bg-green-600 text-white w-full py-3 rounded-md flex justify-center items-center gap-2">
-                        <TbClockPin className="text-lg" />
-                        <span>Absen masuk</span>
-                    </Link>
-                    <Link to={{ pathname: "/" }} className="bg-red-600 text-white w-full py-3 rounded-md flex justify-center items-center gap-2">
-                        <RiClockwiseLine className="text-lg" />
-                        <span>Absen pulang</span>
-                    </Link>
+        <main
+            className="font-inter w-full max-w-[1800px] mx-auto min-h-screen flex flex-col md:flex-row"
+            style={{ background: "url('/office.jpg')", backgroundRepeat: "no-repeat", backgroundPosition: "center" }}
+        >
+            <div className="absolute inset-0 bg-black/50" />
+            <section className="w-full md:w-6/12 bg-yellow-500/30 grid place-items-center md:rounded-r-3xl overflow-hidden relative py-5 md:py-0">
+                <img src="/logo.png" alt="logo" />
+                <p className="text-white text-base md:text-xl font-bold">PT Junadev Technology</p>
+            </section>
+            <section
+                className="w-full px-10 md:w-6/12 flex flex-col justify-center items-center gap-5 md:gap-4 relative"
+            >
+                <header className="grid place-items-center relative py-5">
+                    <img src="/berkat-group-icon.png" className="" width={200} />
+                    <div className="text-center -tracking-wide space-y-2">
+                        <h1 className="text-lg font-bold text-yellow-500">SELAMAT DATANG ADMIN</h1>
+                        <p className="text-xs md:text-sm text-white">Jalanin produktifitas anda dengan aplikasi CRM dari Berkat Group.</p>
+                    </div>
+                </header>
+                <div className="w-full md:w-8/12 space-y-4 pb-4">
+                    <div
+                        style={{ background: "url('/cashier.jpg')", backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "600px" }}
+                        className="relative h-28 w-full rounded-lg overflow-hidden grid place-items-center">
+                        <div className="absolute inset-0 bg-black/60" />
+                        <div className="relative text-white z-2 flex gap-3 w-full px-4 md:px-10">
+                            <PiCashRegister className="text-3xl shrink-0 text-orange-300" />
+                            <div className="space-y-1">
+                                <h2 className="text-sm md:text-xl font-bold">KASIR</h2>
+                                <p className="text-[0.65rem] md:text-xs font-light">Kelola data kasir untuk memantau aktivitas dan performa mereka</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        style={{ background: "url('/employee.jpg')", backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "600px" }}
+                        className="relative h-28 w-full rounded-lg overflow-hidden grid place-items-center">
+                        <div className="absolute inset-0 bg-black/60" />
+                        <div className="relative text-white z-2 flex gap-3 w-full px-4 md:px-10">
+                            <GrSchedules className="text-2xl shrink-0 text-yellow-300" />
+                            <div className="space-y-1">
+                                <h2 className="text-sm md:text-xl font-bold">JADWAL KARYAWAN</h2>
+                                <p className="text-[0.65rem] md:text-xs font-light">Pantau jadwal kerja karyawan agar operasional lebih terorganisir</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        style={{ background: "url('/money.jpg')", backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "600px" }}
+                        className="relative h-28 w-full rounded-lg overflow-hidden grid place-items-center">
+                        <div className="absolute inset-0 bg-black/60" />
+                        <div className="relative text-white z-2 flex gap-3 w-full px-4 md:px-10">
+                            <TbReportMoney className="text-3xl shrink-0 text-green-300" />
+                            <div className="space-y-1">
+                                <h2 className="text-sm md:text-xl font-bold">GAJIAN</h2>
+                                <p className="text-[0.65rem] md:text-xs font-light"> kelola gaji karyawan dengan mudah, lengkap</p>
+                            </div>
+                        </div>
+                    </div>
+                    <div
+                        style={{ background: "url('/office.jpg')", backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "600px" }}
+                        className="relative h-28 w-full rounded-lg overflow-hidden grid place-items-center">
+                        <div className="absolute inset-0 bg-black/60" />
+                        <div className="relative text-white z-2 flex gap-3 w-full px-4 md:px-10">
+                            <HiOutlineOfficeBuilding className="text-3xl shrink-0 text-blue-300" />
+                            <div className="space-y-1">
+                                <h2 className="text-sm md:text-xl font-bold">OFFICE</h2>
+                                <p className="text-[0.65rem] md:text-xs font-light">kelola data penting seperti inventaris kantor, daftar karyawan, dan dokumen lainnya.</p>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </section>
-            <footer>
-                <p className="text-[0.6rem] font-medium text-gray-400">2024 @ BERKAT GROUP</p>
-            </footer>
         </main>
     )
-}
+} 
--- src/routes/app-route.tsx
+++ src/routes/app-route.tsx
@@ -1,11 +1,11 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router"
+import { BrowserRouter, Routes, Route } from "react-router"
 import * as Page from "#/pages"
 
 export default function AppRoute() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Navigate to="/absent" />} />
+                <Route path="/" element={<Page.Home />} />
                 <Route path="/absent">
                     <Route index element={<Page.Absent />} />
                     <Route path="clock-out" element={<Page.ClockOut />} />
